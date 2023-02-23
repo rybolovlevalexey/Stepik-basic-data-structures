464678f1be9024484effc844fc972c55ca885cc4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-23 11:09:08 +0300

    Поменять местами минимальный и максимальный элементы

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,15 +7,45 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            string st = Console.ReadLine();
-            int summa = 0;
-            foreach (var elem in st)
+            string[] st = Console.ReadLine().Split();
+            int mn = 0, mx = 0, ind_mn = -1, ind_mx = -1;
+            for (int i = 0; i < st.Length; i++)
             {
-                if (elem == ' ' || elem == '-')
-                    continue;
-                summa += Convert.ToInt32(Convert.ToString(elem));
+                if (ind_mn == -1)
+                {
+                    ind_mn = 0;
+                    ind_mx = 0;
+                    mn = Convert.ToInt32(Convert.ToString(st[i]));
+                    mx = Convert.ToInt32(Convert.ToString(st[i]));
+                } else
+                {
+                    int elem = Convert.ToInt32(Convert.ToString(st[i]));
+                    if (elem < mn)
+                    {
+                        mn = elem;
+                        ind_mn = i;
+                    }
+                    if (elem > mx)
+                    {
+                        mx = elem;
+                        ind_mx = i;
+                    }
+                }
             }
-            Console.WriteLine(summa);
+            for (int i = 0; i < st.Length; i += 1)
+            {
+                if (i == ind_mn && i != ind_mx)
+                {
+                    Console.Write($"{mx} ");
+                }
+                if (i == ind_mx && i != ind_mn)
+                {
+                    Console.Write($"{mn} ");
+                }
+                if (i != ind_mn && i != ind_mx){
+                    Console.Write($"{st[i]} ");
+                }
+            }
         }
     }
 }
