f335103a8c92116a7f75f2cfeac103a51507abb3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-24 10:57:33 +0300

    в класс Deque добавлены push_back/front

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,13 +17,41 @@ namespace ConsoleApp1
 
     class Deque
     {
+        DequeElem Head = null;
 
+        public void push_back(string elem)
+        {
+            if (Head is null)
+                Head = new DequeElem(elem);
+            else
+            {
+                DequeElem cur = Head;
+                while (!(cur.next is null))
+                    cur = cur.next;
+                cur.next = new DequeElem(elem);
+            }
+            Console.WriteLine("ok");
+        }
+
+        public void push_front(string elem)
+        {
+            if (Head is null)
+                Head = new DequeElem(elem);
+            else
+            {
+                DequeElem cur = Head;
+                DequeElem new_elem = new DequeElem(elem);
+                new_elem.next = cur;
+                Head = new_elem;
+            }
+            Console.WriteLine("ok");
+        }
     }
 
     class DequeElem
     {
-        string value;
-        DequeElem prev = null, next = null;
+        public string value;
+        public DequeElem prev = null, next = null;
 
         public DequeElem(string x)
         {
