f42a2b9c9f3c4da05c056f4a6c8269fe0fcf3150
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-25 13:15:54 +0300

    Максимальное количество последовательных единиц

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,38 +7,19 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            string[] st = Console.ReadLine().Split();
-            int[] sp = new int[n];
-            for (int i = 0; i < n; i++)
-                sp[i] = Convert.ToInt32(st[i]);
-            int mn = 0, mx = 0, mni = 0, mxi = 0;
-            for (int i = 0; i < n; i++)
+            string st = Console.ReadLine();
+            while (st.Contains(" "))
+                st = st.Replace(" ", "");           
+            while (st.Contains("00"))
+                st = st.Replace("00", "0");
+            string[] stroka = st.Split("0");
+            int ans = 0;
+            foreach (var elem in stroka)
             {
-                if (i == 0 || sp[i] > mx)
-                {
-                    mx = sp[i];
-                    mxi = i;
-                }
-                if (i == 0 || sp[i] < mn)
-                {
-                    mn = sp[i];
-                    mni = i;
-                }
+                if (elem.Length > ans)
+                    ans = elem.Length;
             }
-            int summa = 0, a= 0 , b = 0;
-            if (mni < mxi)
-            {
-                a = mni;
-                b = mxi;
-            }
-            if (mni > mxi)
-            {
-                a = mxi;
-                b = mni;
-            }
-            for (int i = a + 1; i < b; i++)
-                summa += sp[i];
-            Console.WriteLine(summa);
+            Console.WriteLine(ans);
         }
     }
 }
