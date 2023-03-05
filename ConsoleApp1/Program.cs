77bee4e72232b33103f7374a0ab9c506e5ece71d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-05 11:11:46 +0300

    Счастливые билеты

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,74 +3,22 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
-    public class Node
-    {
-        public Node Next;
-        public int Value;
-
-        public Node(int value)
-        {
-            Value = value;
-        }
-    }
-    public class SinglyLinkedList
-    {
-        public Node Head;
-        private int count = 0;
-        public Node FindByIndex(int index)
-        {
-            // реализуйте данный метод
-            int ind = 0;
-            Node cur = Head;
-            while (cur != null)
-            {
-                if (ind == index)
-                    return cur;
-                cur = cur.Next;
-                ind += 1;
-            }
-            return new Node(-1);
-        }
-        public Node FindLast(int key)
-        {
-            Node ans = null;
-            Node cur = Head;
-            while (cur != null)
-            {
-                if (cur.Value == key)
-                    ans = cur;
-                cur = cur.Next;
-            }
-            return ans;
-        }
-        public int GetCount()
-        { }
-
-        public string Print()
-        {
-            
-        }
-
-        public Node Find(int key)
-        {
-            return new Node(-1);
-        }
-    }
-    class Program
+	class Program
     {
         static void Main(string[] args)
         {
-            List<int> sp = new List<int>();
-            int x = Convert.ToInt32(Console.ReadLine());
-            while (x != 0)
+            static bool IsGood(string numb)
             {
-                sp.Add(x);
-                x = Convert.ToInt32(Console.ReadLine());
+                if (Convert.ToInt32(numb[0]) + Convert.ToInt32(numb[1]) + Convert.ToInt32(numb[2]) == Convert.ToInt32(numb[3]) + Convert.ToInt32(numb[4]) + Convert.ToInt32(numb[5]))
+                    return true;
+                return false;
             }
-            for (int i = sp.Count - 1; i >= 0; i -= 1)
-            {
-                Console.Write($"{sp[i]} ");
-            }
+            string x1 = (Console.ReadLine());
+            string x2 = (Console.ReadLine());
+            if (IsGood(x1) && IsGood(x2))
+                Console.WriteLine(1);
+            else
+                Console.WriteLine(-1);
         }
     }
 }
