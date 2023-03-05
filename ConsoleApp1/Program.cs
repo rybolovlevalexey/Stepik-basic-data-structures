4a9c442d2ecc7fe7ffa22add3e5a275ba4080e0d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-05 10:33:26 +0300

    FindLast/FindByIndex

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,59 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
+    public class Node
+    {
+        public Node Next;
+        public int Value;
+
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+    public class SinglyLinkedList
+    {
+        public Node Head;
+        private int count = 0;
+        public Node FindByIndex(int index)
+        {
+            // реализуйте данный метод
+            int ind = 0;
+            Node cur = Head;
+            while (cur != null)
+            {
+                if (ind == index)
+                    return cur;
+                cur = cur.Next;
+                ind += 1;
+            }
+            return new Node(-1);
+        }
+        public Node FindLast(int key)
+        {
+            Node ans = null;
+            Node cur = Head;
+            while (cur != null)
+            {
+                if (cur.Value == key)
+                    ans = cur;
+                cur = cur.Next;
+            }
+            return ans;
+        }
+        public int GetCount()
+        { }
+
+        public string Print()
+        {
+            
+        }
+
+        public Node Find(int key)
+        {
+            return new Node(-1);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
