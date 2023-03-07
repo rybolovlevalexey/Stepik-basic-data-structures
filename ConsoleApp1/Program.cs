fff316741f3cc399fa598c5b2c4bfab9dafe71ee
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-07 21:53:40 +0300

    Анаграммы

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -17,12 +17,31 @@ namespace ConsoleApp1
                 else
                     letters[elem] = 1;
             }
-            List<string> keys = new List<string>(letters.Keys);
-            keys.Sort();
-            foreach(string key in keys)
+            string st1 = Console.ReadLine();
+            bool flag = true;
+            for (int i = 0; i < st1.Length; i += 1)
             {
-                Console.WriteLine($"{key} {letters[key]}");
+                string elem = Convert.ToString(st1[i]);
+                if (letters.ContainsKey(elem))
+                {
+                    if (letters[elem] == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                    else
+                        letters[elem] -= 1;
+                }
+                else
+                {
+                    flag = false;
+                    break;
+                }
             }
+            if (flag && st.Length == st1.Length)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
         }
     }
 }
