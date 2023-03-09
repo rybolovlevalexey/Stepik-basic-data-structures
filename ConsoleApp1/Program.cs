19e0c595404d249e87694bc134ee0707d6ec577b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-09 20:44:37 +0300

    Удаление всех элементов

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,28 +3,132 @@ using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
+	public class DynamicArray
+	{
+		private int[] items;
+
+		private int size = 0;
+		public int RemoveAll(int item)
+		{
+			// реализуйте данный метод
+			int cnt = 0;
+			int ind = 0;
+			while (ind < size)
+            {
+				if (items[ind] == item)
+                {
+					this.RemoveByIndex(ind);
+					cnt += 1;
+                }
+					
+				else
+					ind += 1;
+            }
+			return cnt;
+		}
+		public DynamicArray(int length)
+		{
+			items = new int[length];
+		}
+		public DynamicArray()
+		{
+			items = new int[0];
+		}
+
+		public string Print()
+		{
+			string result = "";
+			for (int i = 0; i < size; i++)
+			{
+				result += items[i] + " ";
+			}
+
+			return result;
+		}
+
+		public int GetCount()
+		{
+			return size;
+		}
+
+		public int Get(int index)
+		{
+			if (index >= size || index < 0)
+			{
+				throw new Exception("Индекс находится за пределами массива.");
+			}
+
+			return items[index];
+		}
+
+		public int Find(int key)
+		{
+			for (int i = 0; i < size; i++)
+			{
+				if (items[i] == key)
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+
+		public void PushBack(int item)
+		{
+			if (size == items.Length)
+			{
+				IncreaseArray();
+			}
+
+			items[size] = item;
+			size++;
+		}
+
+		private void IncreaseArray()
+		{ }
+
+		public void Insert(int index, int item)
+		{
+			if (index < 0 || index > size)
+			{
+				throw new Exception("Выход за пределами массива");
+			}
+
+			if (size == items.Length)
+			{
+				IncreaseArray();
+			}
+
+			// сдвигаем все элементы вправо до нужного индекса
+			for (int i = size - 1; i >= index; i--)
+			{
+				items[i + 1] = items[i];
+			}
+
+			items[index] = item;
+			size++;
+		}
+
+		public void PushFront(int item)
+		{
+			Insert(0, item);
+		}
+
+		public void PopBack()
+		{ }
+
+		public void RemoveByIndex(int index)
+		{ }
+
+		public void PopFront()
+		{ }
+	}
 	class Program
     {
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
-            Dictionary<string, int> persons = new Dictionary<string, int>();
-            for (int i = 0; i < n; i += 1)
-            {
-                string st = Console.ReadLine();
-                bool flag = true;
-                if (!persons.ContainsKey(st))
-                    flag = false;
-                if (!flag)
-                {
-                    Console.WriteLine("OK");
-                    persons[st] = 1;
-                } else
-                {
-                    Console.WriteLine(st + Convert.ToString(persons[st]));
-                    persons[st] += 1;
-                }
-            }    
+                
         }
     }
 }
