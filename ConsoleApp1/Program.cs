4ab31b263525f730e3d024fdb096f652609548e8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-15 09:39:15 +0300

    BeeGeek Dictionary

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,39 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
+
 
 namespace ConsoleApp1
 {
-    //1 - I, 5 - V, 10 - X, 50 - L, 100 - C, 500 - D и 1000 - M.
-    //MCMXCIV
     class Program
     {
-        public static string LongestCommonPrefix(string[] strs)
-        {
-            string ans = strs[0];
-            for (int i = 0; i < strs.Length; i += 1)
-            {
-                int dl = 0;
-                if (ans.Length == 0)
-                    return ans;
-                foreach (var elem in strs[i])
-                {
-                    if (dl >= ans.Length)
-                        break;
-                    if (elem == ans[dl])
-                        dl += 1;
-                    else
-                        break;
-                }
-                if (dl == 0)
-                    return "";
-                ans = ans[0..dl];
-            }
-            return ans;
-        }
         static void Main(string[] args)
         {
-            string[] array = Console.ReadLine().Split();
-            Console.WriteLine(LongestCommonPrefix(array));
+            int n = Convert.ToInt32(Console.ReadLine());
+            Dictionary<string, string> words = new Dictionary<string, string>();
+            for(int i = 0; i < n; i += 1)
+            {
+                string st = Console.ReadLine();
+                int ind = st.IndexOf(" ");
+                words[st[0..ind]] = st[(ind+1)..];
+            }
+            int m = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < m; i += 1)
+            {
+                string st = Console.ReadLine();
+                bool flag = false;
+                foreach (var key in words.Keys)
+                {
+                    if (key == st)
+                    {
+                        flag = true;
+                        Console.WriteLine(words[key]);
+                    }
+                }
+                if (!flag)
+                    Console.WriteLine("Нет в словаре");
+            }
         }
     }
 }
