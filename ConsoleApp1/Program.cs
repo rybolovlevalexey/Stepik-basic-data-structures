12ef1f125e81f7cb8a1cbb1c54bc8c8ff70ff741
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-15 11:15:36 +0300

    Очередь

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -44,12 +44,49 @@ namespace ConsoleApp1
         {
             Console.WriteLine(size);
         }
+        public void clear()
+        {
+            size = 0;
+            items = new List<string>();
+            Console.WriteLine("ok");
+        }
+        public void exit()
+        {
+            Console.WriteLine("bye");
+        }
     }
     class Program
     {
 		static void Main(string[] args)
         {
-            
+            Queue que = new Queue();
+            string st = Console.ReadLine();
+            while (st != "exit")
+            {
+                if (st.Contains(" "))
+                {
+                    que.enqueue(st.Split()[1]);
+                } else
+                {
+                    switch (st)
+                    {
+                        case "dequeue":
+                            que.dequeue();
+                            break;
+                        case "peek":
+                            que.peek();
+                            break;
+                        case "count":
+                            que.count();
+                            break;
+                        case "clear":
+                            que.clear();
+                            break;
+                    }
+                }
+                st = Console.ReadLine();
+            }
+            que.exit();
         }
     }
 }
