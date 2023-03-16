b333e35e7a7ae7eedf0a7309ecb8084a9596b151
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-16 09:28:20 +0300

    Переключение между окнами

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,16 +10,26 @@ namespace ConsoleApp1
 		static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            int step = n - 1;
-            string[] st = Console.ReadLine().Split();
-            int ans = 0;
-            for (int i = 0; i < n; i += 1)
+            LinkedList<string> apps = new LinkedList<string>();
+            for(int i = 0; i < n; i += 1)
             {
-                if (st[i] == "1")
-                    ans += Convert.ToInt32(Math.Pow(2, step));
-                step -= 1;
+                string st = Console.ReadLine();
+                if (st.StartsWith("Run"))
+                {
+                    string name = st[4..];
+                    apps.AddFirst(name);
+                } else
+                {
+                    string[] deyst = st.Split("+");
+                    int ind = (deyst.Length - 1) % apps.Count + 1;
+                    LinkedListNode<string> cur = apps.First;
+                    for (int j = 0; j < ind - 1; j += 1)
+                        cur = cur.Next;
+                    apps.Remove(cur);
+                    apps.AddFirst(cur);
+                }
+                Console.WriteLine(apps.First.Value);
             }
-            Console.WriteLine(ans);
         }
     }
 }
