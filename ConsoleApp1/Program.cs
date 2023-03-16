f340132c0d5533f0244ad41a56c925077aa3ad94
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-16 22:20:05 +0300

    Ближайший бОльший элемент

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,19 +10,24 @@ namespace ConsoleApp1
 		static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            LinkedList<string> ans = new LinkedList<string>();
-            string[] st = Console.ReadLine().Split();
-            string last = "";
-            foreach(string elem in st)
+            LinkedList<string> elements = new LinkedList<string>(Console.ReadLine().Split());
+            Stack<(int, int)> stack = new Stack<(int, int)>();  // tuple(value, index)
+            int[] ans = new int[elements.Count];
+            int ind = 0;
+            foreach(var elem in elements)
             {
-                if (elem != last)
+                int value = Convert.ToInt32(elem);
+                while (stack.Count > 0 && value > stack.Peek().Item1)
                 {
-                    ans.AddLast(elem);
-                    last = elem;
+                    ans[stack.Peek().Item2] = value;
+                    stack.Pop();
                 }
+                (int, int) tup = (value, ind);
+                stack.Push(tup);
+                ind += 1;
             }
-            foreach (var elem in ans)
-                Console.Write(elem + " ");
+            foreach (var el in ans)
+                Console.Write(el + " ");
         }
     }
 }
