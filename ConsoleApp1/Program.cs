9ca1155b29793a27d02a6473003203fbde709bc3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-26 11:49:31 +0300

    Merge Two Sorted Lists

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,50 +9,94 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Solution sl = new Solution("()[]{ }");
+            ListNode l1 = new ListNode(1);
+            l1.next = new ListNode(2);
+            ListNode l2 = new ListNode(1);
+            l2.next = new ListNode(3);
+            Solution sl = new Solution(l1, l2);
 
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
         }
     }
     public class Solution
     {
-        public Solution(string st)
+        public Solution(ListNode list1, ListNode list2)
         {
-            Console.WriteLine(IsValid(st));
+            Console.WriteLine(MergeTwoLists(list1, list2));
         }
-        public bool IsValid(string s)
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
         {
-            Stack<char> stck = new Stack<char>();
-            foreach (var el in s)
+            ListNode Head = new ListNode();
+            if (list1 is null && list2 is null)
+                return null;
+            if (list1 != null && list2 != null)
             {
-                if (!"{}[]()".Contains(el))
-                    continue;
-                if (el == '{' || el == '(' || el == '[')
-                    stck.Push(el);
+                if (list1.val < list2.val)
+                {
+                    Head.val = list1.val;
+                    list1 = list1.next;
+                } else
+                {
+                    Head.val = list2.val;
+                    list2 = list2.next;
+                }
+            } else
+            {
+                if (list1 is null)
+                {
+                    Head.val = list2.val;
+                    list2 = list2.next;
+                }
                 else
                 {
-                    char elem = '0';
-                    switch (el)
-                    {
-                        case ')':
-                            elem = '(';
-                            break;
-                        case '}':
-                            elem = '{';
-                            break;
-                        case ']':
-                            elem = '[';
-                            break;
-                    }
-                    if (stck.Count == 0 || Convert.ToString(elem) != Convert.ToString(stck.Peek()))
-                    {
-                        return false;
-                    }
-                        
-                    else
-                        stck.Pop();
+                    Head.val = list1.val;
+                    list1 = list1.next;
                 }
             }
-            return stck.Count == 0;
+
+            ListNode cur_head = Head;
+            while (list1 != null || list2 != null)
+            {
+                if (list1 != null && list2 != null)
+                {
+                    if (list1.val < list2.val)
+                    {
+                        ListNode cur = new ListNode(list1.val);
+                        list1 = list1.next;
+                        cur_head.next = cur;
+                    } else
+                    {
+                        ListNode cur = new ListNode(list2.val);
+                        list2 = list2.next;
+                        cur_head.next = cur;
+                    }
+                    cur_head = cur_head.next;
+                } else
+                {
+                    if (list1 is null)
+                    {
+                        ListNode cur = new ListNode(list2.val);
+                        list2 = list2.next;
+                        cur_head.next = cur;
+                    } else
+                    {
+                        ListNode cur = new ListNode(list1.val);
+                        list1 = list1.next;
+                        cur_head.next = cur;
+                    }
+                }
+                
+            }
+            return Head;
         }
     }
 }
